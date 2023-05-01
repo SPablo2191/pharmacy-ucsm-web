0819810c494d10a70094d4cb255a20ff10b6bcd0
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-01 16:53:31 -0500

    feat: uiInputText component added

---
+++ src/app/shared/ui-input-text/ui-input-text.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UiInputTextComponent } from './ui-input-text.component';
+
+describe('UiInputTextComponent', () => {
+  let component: UiInputTextComponent;
+  let fixture: ComponentFixture<UiInputTextComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ UiInputTextComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UiInputTextComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/ui-input-text/ui-input-text.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ui-input-text',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      ui-input-text works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class UiInputTextComponent {
+
+}
