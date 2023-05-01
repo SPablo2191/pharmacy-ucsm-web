b03edf15fc0e601ca281c1eac051c29313c67819
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-01 13:17:21 -0500

    feat: "home" module in process

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MenubarModule],
+  template: `
+    <div>
+      <p-menubar [model]="items"></p-menubar>
+    </div>
+  `,
+  styles: [],
+})
+export class NavbarComponent {
+  items = [
+    {
+      label: 'Inicio',
+      icon: 'pi pi-home',
+    },
+    {
+      label: 'Sobre Nosotros',
+      icon: 'pi pi-users',
+    },
+    {
+      label: 'Productos',
+      icon: 'pi pi-box',
+    },
+
+  ];
+}
