6e54177d9162e7f93fcd6a71ecd1be65dc7161cc
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-06 12:57:05 -0500

    feat: receipt service added

--- src/app/services/receipt.service.ts
+++ src/app/services/receipt.service.ts
@@ -3,13 +3,18 @@ import { BaseService } from './base.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { pathnameEnum } from '../project/enums/pathname.enum';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReceiptService extends BaseService{
-  override serverUrl = `${environment.apiUrl}${pathnameEnum.movies}`;
+  override serverUrl = `${environment.apiUrl}${pathnameEnum.receipt}`;
   constructor(httpClient: HttpClient) {
     super(httpClient);
+  }
+  override get(params = {}) : Observable<any []> {
+    const url = `${environment.apiUrl}${pathnameEnum.receipts}`
+    return this.httpClient.get<any []>(url,{params});
   }
 }
