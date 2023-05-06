cd065096887b120b5c607666ad7ac78f68904002
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-06 16:30:56 -0500

    feat: dropdown component added

---
+++ src/app/shared/ui-dropdown/ui-dropdown.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-ui-dropdown',
+  standalone: true,
+  imports: [CommonModule, DropdownModule, ReactiveFormsModule],
+  template: `
+    <div class="field">
+      <form [formGroup]="group">
+        <label [for]="name" class="text-gray-700">{{ label }}</label>
+        <div class="p-inputgroup">
+          <p-dropdown
+            [formControlName]="name"
+            [options]="options"
+            [optionLabel]="optionLabel"
+          ></p-dropdown>
+        </div>
+      </form>
+    </div>
+  `,
+  styles: [],
+})
+export class UiDropdownComponent {
+  @Input() group!: FormGroup;
+  @Input() name!: string;
+  @Input() label!: string;
+  @Input() options!: any[];
+  @Input() optionLabel!: string;
+}
