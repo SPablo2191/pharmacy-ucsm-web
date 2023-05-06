504b157d3ff20fbe722193780b3b103d60e5218c
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-06 16:23:50 -0500

    feat: add email control

--- src/app/modules/add-receipt/add-receipt.component.ts
+++ src/app/modules/add-receipt/add-receipt.component.ts
@@ -18,6 +18,7 @@ export class AddReceiptComponent extends abstractForm implements OnInit {
       phoneNumber: [],
       DNI: [],
       address: [],
+      email: [],
     });
   }
   override submit(): void {}
