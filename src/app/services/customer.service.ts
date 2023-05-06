233a7590036c2e0b7b18e93e3484fbfd46676999
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-06 13:24:25 -0500

    feat: customer service working

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { BaseService } from './base.service';
+import { environment } from 'src/environments/environment';
+import { pathnameEnum } from '../project/enums/pathname.enum';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CustomerService {
-
-  constructor() { }
+export class CustomerService extends BaseService {
+  override serverUrl = `${environment.apiUrl}${pathnameEnum.customer}`;
+  constructor(httpClient: HttpClient) {
+    super(httpClient);
+  }
+  override get(params = {}) : Observable<any []> {
+    const url = `${environment.apiUrl}${pathnameEnum.customers}`
+    return this.httpClient.get<any []>(url,{params});
+  }
 }
