9f27cfa2bf8be2e08da535ac98060d46a391b2ee
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-05-07 19:31:41 -0500

    fix: addStock component added

---
+++ src/app/modules/inventory/components/add-stock/add-stock.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-stock',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      add-stock works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AddStockComponent {
+
+}
